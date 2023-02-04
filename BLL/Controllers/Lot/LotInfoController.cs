4c4177d3522eb40aa3c2f1bc30c92b6e13999261
tobias-roy <tobiasroy94@gmail.com>
2023-02-04 21:24:42 +0100

    Lotinfo screen now shows correctly

--- BLL/Controllers/Lot/LotInfoController.cs
+++ BLL/Controllers/Lot/LotInfoController.cs
@@ -17,11 +17,10 @@ namespace BLL.Controllers
       int id = _ticketRepository.GetLatestID();
       Ticket yourTicket = _ticketRepository.GetTicketByID(id);
       DateTime parkingStart = Convert.ToDateTime(yourTicket.ParkingStart);
-
       Console.WriteLine($"Kør til plads {yourTicket.LotID}");
       Console.WriteLine($"Prisen i pr. påbegyndt time er {yourTicket.Price} kr.-");
-      Console.WriteLine($"Din parkering er registreret påbegyndt {parkingStart.ToString("dddd, dd MMMM yyyy")}");
-      Console.ReadKey();
+      Console.WriteLine($"Din parkering er registreret påbegyndt {parkingStart.ToString("dddd, dd MMMM yyyy HH:mm:ss")}");
+      Thread.Sleep(8000);
     }
   }
 }
